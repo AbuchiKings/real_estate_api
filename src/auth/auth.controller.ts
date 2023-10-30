ed4ec16c1aec86189207b65962c5047d03163e23
abuchikings <abuchikingsley76@gmail.com>
2023-10-30 20:22:24 +0100

    Updated auth controller to use user service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { newUser } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('signup')
+    @HttpCode(HttpStatus.CREATED)
+    async signUp(@Body() data: newUser) {
+        return this.authService.signUp(data);
+    }
+
+}
